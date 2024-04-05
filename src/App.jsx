2fbcac9f6a2ae59248b4dc247781019645416ffe
Raaja Kartikeya <raaja@Raajas-MacBook-Air.local>
2024-04-05 20:35:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './index.css'
+export default function App(){
+  return (
+    <>
+      <header className='header'>
+        <a id='name' href="#home">Raaja 
+        <span id='sname'  > Kartikeya</span></a>
+        <nav className='navbar'>
+          <a href="#home" className='active'>Home </a>
+          <a href="#skills">Skills </a>
+          <a href="#projects">Projects </a>
+          <a href="#projects">Resume </a>
+          <a href="#contact">Contact </a>
+        </nav>
+      </header>
+
+      <div className='image'>
+        <img src="images/img2.png" alt="" />
+      </div>
+     <h1 className='h1name'>I'm <span>Raaja Kartikeya</span></h1>
+     <p>To land a demanding job in a field where I can apply my knowledge,
+      expertise, and enthusiasm to successfully support the organisation's
+      success while advancing my own development on both a personal
+      and professional level.</p>
+
+      <div className='socialicons'>
+        <a href="https://github.com/raajakartikeya" target='_blank' ><i className='bx bxl-github' ></i></a>
+        <a href="https://www.linkedin.com/in/raajakartikeya/" target='_blank'><i className='bx bxl-linkedin' ></i></a>
+      </div>
+
+    </>
+  );
+}
